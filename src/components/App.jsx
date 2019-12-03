475fcb7b3e455bdb9ec80b906d6665fa711ae2fc
justin <justin0979@gmail.com>
2019-12-03 00:11:57 -0600

    added all edited photos from gimp, experiment with button to scale images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import PictureCard from '@components/PictureCard';
 import apePicChimp from '@images/edited_apePicChimp';
+import asianElephant from '@images/edited_asianElephant';
+import birdBank from '@images/edited_birdBank';
+/*
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+import apePicChimp from '@images/edited_apePicChimp';
+*/
 
 const App = () => {
   return (
     <div className="app">
       <PictureCard src={apePicChimp} text="Taking time to be an ape" />
-      <PictureCard src={apePicChimp} text="From App" />
-      <PictureCard src={apePicChimp} text="From App" />
+      <PictureCard src={asianElephant} text="Asian Elephant Exhibit " />
+      <PictureCard src={birdBank} text="A bird bank" />
       <PictureCard src={apePicChimp} text="From App" />
       <PictureCard src={apePicChimp} text="From App" />
       <PictureCard src={apePicChimp} text="From App" />
