125b0cf33859b7226cb17c2be2c53613747459d7
justin0979 <justinm0979@gmail.com>
2020-10-26 14:41:11 -0500

    add favicon and delete dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,77 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ['./src'],
+  entry: ["./src"],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: ''
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
   },
-  mode: 'development',
+  mode: "development",
   optimization: {
     splitChunks: {
-      chunks: 'all'
-    }
+      chunks: "all",
+    },
   },
   devServer: {
-    contentBase: 'dist',
+    contentBase: "dist",
     port: 3000,
     hot: true,
-    overlay: true
+    overlay: true,
   },
   resolve: {
-    modules: ['src', 'src/images', 'node_modules'],
+    modules: ["src", "src/images", "node_modules"],
     extensions: [
-      '.js',
-      '.jsx',
-      '.css',
-      '.scss',
-      '.jpg',
-      '.jpeg',
-      '.png',
-      '.svg',
-      '.gif'
-    ]
+      ".js",
+      ".jsx",
+      ".css",
+      ".scss",
+      ".jpg",
+      ".jpeg",
+      ".png",
+      ".svg",
+      ".gif",
+    ],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader",
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.html$/,
-        use: 'html-loader'
+        use: "html-loader",
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[path][name].[ext]'
-            }
+              name: "[path][name].[ext]",
+            },
           },
-          'image-webpack-loader'
-        ]
-      }
-    ]
+          "image-webpack-loader",
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+      template: "./src/index.html",
+      favicon: "./src/lastname-32x32.png",
+    }),
+  ],
 };
